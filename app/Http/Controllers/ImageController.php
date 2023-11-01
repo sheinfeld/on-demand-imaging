7cbf5a525cbc85c7c3b0ed093b8842a3d8447154
lopes <lopes.tomasc@ceuton.net>
2023-11-01 16:28:10 +0000

    Initial

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\File;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class ImageController extends Controller
+{
+    public function index(Request $request)
+    {
+        try {
+            $request->validate([
+                'url' => 'required|string',
+                'format' => 'required|string',
+            ]);
+
+            $image = file_get_contents($request->get('url'));
+            return Image::make($image)->encode($request->get('format'))->response();
+
+        } catch (\Throwable $th) {
+            abort(500, $th->getMessage());
+        }
+    }
+}
